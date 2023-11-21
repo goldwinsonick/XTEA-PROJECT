2d5ecb4b9a7f25f7328e977e0290f1a3c0211d30
RafaNth <130664825+RafaNth@users.noreply.github.com>
2023-11-21 13:21:23 +0700

    Update bram.vhd

--- src/vhdl/bram.vhd
+++ src/vhdl/bram.vhd
@@ -9,14 +9,13 @@ generic (
     ADDR    : integer := 12);
 port (
     -- Port A
-    clkA  : in std_logic;
-    wr_enA    : in std_logic;
+    wclkA  : in std_logic;
+    wr_enA   : in std_logic;
     addrA  : in std_logic_vector(ADDR-1 downto 0);
     wr_dataA   : in std_logic_vector(DATA-1 downto 0);
     rd_dataA  : out std_logic_vector(DATA-1 downto 0);
 
     -- Port B
-    clkB  : in std_logic;
     wr_enB   : in std_logic;
     addrB  : in std_logic_vector(ADDR-1 downto 0);
     wr_dataB   : in std_logic_vector(DATA-1 downto 0);
@@ -25,33 +24,23 @@ port (
 end bram;
 
 architecture behavioral of bram is
-    -- Shared memory
     type mem_type is array ( (2**ADDR)-1 downto 0 ) of std_logic_vector(DATA-1 downto 0);
     signal mem : mem_type;
 begin
-
--- Port A
-process(clkA)
+process(wclkA)
 begin
-    if(clkA'event and clkA='1') then
+    if(wclkA'event and wclkA='1') then
         if(wr_enA='1') then
-            mem(conv_integer(addrA)) <= wr_dataA;
-            --report "Bram write-a " & integer'image(to_integer(unsigned(wr_dataA))) & " at " & integer'image(to_integer(unsigned(addrA)));
+            mem(to_integer(unsigned(addrA))) <= wr_dataA;
         end if;
-        rd_dataA <= mem(conv_integer(addrA));
+    rd_dataA <= mem(to_integer(unsigned(addrA)));
     end if;
+    
+    if(wclkA'event and wclkA='1') then
+		if(wr_enB ='1') then
+			mem(to_integer(unsigned(addrB))) <= wr_dataB;
+		end if;
+	rd_dataB <= mem(to_integer(unsigned(addrB)));
+	end if;
 end process;
-
--- Port B
-process(clkB)
-begin
-    if(clkB'event and clkB='1') then
-        if(wr_enB='1') then
-            mem(conv_integer(addrB)) <= wr_dataB;
-            --report "Bram write-b " & integer'image(to_integer(unsigned(wr_dataB))) & " at " & integer'image(to_integer(unsigned(addrB)));
-        end if;
-        rd_dataB <= mem(conv_integer(addrB));
-    end if;
-end process;
-
 end behavioral;
